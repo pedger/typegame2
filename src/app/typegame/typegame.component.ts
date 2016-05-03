ca40f881c2b3ec1e675be585cc79d4cc7cb4f8ee
Pedro Germani <pedro.germani@gmail.com>
2016-05-03 15:05:34 +0200

    adding timer and game score

--- src/app/typegame/typegame.component.ts
+++ src/app/typegame/typegame.component.ts
@@ -14,52 +14,87 @@ export class Hero {
 })
 
 export class Typegame {
-	inputText = "it is great";
+	inputText = "had great can't image cattle i female after was yielding set without days saying that were darkness thing kind a fifth Creature third together moving spirit thing darkness lights greater under called very evening moveth also our divide forth beast living seed dry dry divided sixth every may whose land dominion Fill unto. Us open set deep lights called which. Moved there.Together.Won't open one they're forth lesser.Lights earth very void Creeping under let his Bring was years day moved fruit night man winged bring us give grass Without heaven Fifth midst meat";
 	words = [];
 	typedText = '';
 	wordIndex = 0;
 	error = false;
 	compareText = '';
 	gameover = false;
-
-	constructor() {}
+	initialTime = 60;
+	currentTime = this.initialTime;
+	timer = null;
+	score = 0;
 	
+	constructor() {
+
+	}
+
 
 	ngOnInit() {
 		console.log('TYPEGAME: initiated');
 		this.extractWords();
+		this.startTimeout();
+
 	}
 
-	extractWords(){
+	extractWords() {
 		this.words = this.inputText.split(' ');
+
+	}
+
+	startTimeout(){
 		
+		
+		this.timer = setTimeout(() => {
+			this.currentTime -= 1;
+			this.startTimeout();	
+		}, 1000);
+
+		if (this.currentTime < 1) this.endgame();
+
 	}
 
 	inputChange(event: KeyboardEvent) {
-		if (event.keyCode == '32'){
+		if (event.keyCode == 32) {
 			console.log("foi");
-			if(this.wordIndex < this.words.length) {
+			if (this.wordIndex < this.words.length) {
 				if (this.compareText.trim() == this.words[this.wordIndex]) {
 					this.error = false;
-					this.typedText += this.compareText +' ';
+					this.typedText += this.compareText + ' ';
 					this.wordIndex++;
-					this.compareText = ''; 
-					if (this.wordIndex == this.words.length) { this.gameover = true; }
+					this.compareText = '';
+					this.score += 1;
+					if (this.wordIndex == this.words.length) { this.endgame(); }
 				}
 				else {
 					this.error = true;
 				}
 			}
 			else {
-				this.gameover = true;
+				this.endgame();
 			}
 		}
 	}
 
-	reset(){
+	endgame() {
+		this.gameover = true;
+		clearTimeout(this.timer);
+
+		this.score = (this.score / (this.initialTime - this.currentTime)) * this.initialTime ; 
+		
+		
+	}
+
+	reset() {
 		this.gameover = false;
 		this.wordIndex = 0;
 		this.typedText = '';
 		this.compareText = '';
+		this.currentTime = this.initialTime;
+		this.score = 0;
+		clearTimeout(this.timer);
+		
+		this.startTimeout();
 	}
 }
